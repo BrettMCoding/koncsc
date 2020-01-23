b9a9814396e1cc20cf60f459ff0f3c68c135c66e
Brett Marsh <brett.m.coding@gmail.com>
2020-01-23 03:40:55 -0500

    Updates

---
+++ precache-manifest.b84cf3b41a3b1748cb07eda3ba3828ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70282f5c2f5b40e30970ef42b3909b20",
+    "url": "/koncsc/index.html"
+  },
+  {
+    "revision": "333b1e6c8ba5c59e8677",
+    "url": "/koncsc/static/css/2.7aa6f31f.chunk.css"
+  },
+  {
+    "revision": "08203a367bf3e39d9342",
+    "url": "/koncsc/static/css/main.a266ee18.chunk.css"
+  },
+  {
+    "revision": "333b1e6c8ba5c59e8677",
+    "url": "/koncsc/static/js/2.e4f0c588.chunk.js"
+  },
+  {
+    "revision": "08203a367bf3e39d9342",
+    "url": "/koncsc/static/js/main.1f46da52.chunk.js"
+  },
+  {
+    "revision": "71a68eafa23b6ebf62f7",
+    "url": "/koncsc/static/js/runtime-main.a531ac47.js"
+  }
+]);
