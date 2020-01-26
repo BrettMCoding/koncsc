e895b726fe611af15302ae032eb2d063d6d74b26
Brett Marsh <brett.m.coding@gmail.com>
2020-01-26 09:24:44 -0500

    Updates

--- precache-manifest.25bf20ae337ccf39a9e1b5b1169f28b0.js
+++ precache-manifest.25bf20ae337ccf39a9e1b5b1169f28b0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8378798a150e1087aeea647bf3d22e21",
+    "revision": "00e872307e8263f48df522e6d370cee5",
     "url": "/koncsc/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/koncsc/static/css/2.7aa6f31f.chunk.css"
   },
   {
-    "revision": "5c7700eaddd730e04607",
-    "url": "/koncsc/static/css/main.fdb1d915.chunk.css"
+    "revision": "618b14162f04d2c4081a",
+    "url": "/koncsc/static/css/main.664c9fae.chunk.css"
   },
   {
     "revision": "3ac5365a7a49fda81e48",
     "url": "/koncsc/static/js/2.d89f6526.chunk.js"
   },
   {
-    "revision": "5c7700eaddd730e04607",
-    "url": "/koncsc/static/js/main.13262f45.chunk.js"
+    "revision": "618b14162f04d2c4081a",
+    "url": "/koncsc/static/js/main.feac5d9c.chunk.js"
   },
   {
     "revision": "71a68eafa23b6ebf62f7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/koncsc/precache-manifest.af19f8e626ec750576bd44734093c1fd.js"
+  "/koncsc/precache-manifest.25bf20ae337ccf39a9e1b5b1169f28b0.js"
 );
 
 self.addEventListener('message', (event) => {
